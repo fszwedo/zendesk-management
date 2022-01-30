2d04f5371225bb6634df7199cf13c3345d8ebb7a
yagi527 <yrogergts@gmail.com>
2022-01-30 19:46:43 +0100

    Selecting agents - changes after code review
    Selecting agents - changes after code review

--- agents.js
+++ agents.js
@@ -1,35 +1,28 @@
-import {readFile} from './readFile.js';
+import { readFile } from './readFile.js';
 import { choiceAgents } from './choiceAgents.js';
 const getDayHour = () => {
-    const d = new Date();
-    let day1 = d.toISOString().slice(0, 10);
-//    day1 = '2022-01-21';
-  
-   let  hourCurrent = d.getHours();
-   let result={day: null,hour:null,err: null};
-   result.day = day1;
-   result.hour = hourCurrent;
-   
-   if (d.getDay() == 6 || d.getDay() == 0)  {
-      result.err = 'Weekend - no assignments';
-    }
-    return result;
+  const d = new Date();
+  let day1 = d.toISOString().slice(0, 10);
+  let hourCurrent = d.getHours();
+  let result = { day: day1, hour: hourCurrent, err: null };
+
+  if (d.getDay() == 6 || d.getDay() == 0) {
+     result.err = 'Weekend - no assignments';
+  }
+  return result;
 }
 
-function printAgents(list1){
-  console.log(list1);
+function printAgents(agentsList) {
+  console.log(agentsList);
 }
 
 const app = async () => {
-    let getDate = getDayHour();
-    if(getDate.err) {
-      console.log(getDate.err);
-      return;
-    }
-    
- //  console.log(getDate.day);
-   await readFile('agents2.csv',getDate.day,getDate.hour).then(choiceAgents).then(printAgents);
-   //console.log('a1');
+  let getDate = getDayHour();
+  if (getDate.err) {
+    console.log(getDate.err);
+    return;
+  }
+  await readFile('agents2.csv', getDate.day, getDate.hour).then(choiceAgents).then(printAgents);
 }
 
 app();
--- readFile.js
+++ readFile.js
@@ -1,41 +1,37 @@
 import fs from 'fs';
 import csvparser from 'csv-parser';
- export async function readFile  (fileName,day,hour)  {
-    let result={
-      list: [],day: day,hour: hour
+export async function readFile(fileName, day, hour) {
+    let result = {
+        list: [], day: day, hour: hour
     };
-   return new Promise((resolve,reject) => {
-      fs.createReadStream(fileName)
-      .pipe(csvparser())
-      .on('data', function(data){
-          // {
-              //console.log('data');
-              try {
-                  
-              if (data.date == day) {
-  
-                  result.list.push(data);
-              }
-  
-              }
-  
-              catch(err) {
-                  
-                  console.log('Error: ' + err);
-                  reject(err);
-              }
-          }) //on.data
-          .on('end', () => {
-              try {
-              resolve(result);
-              }
-              catch (error) {
-              console.error(error);
-              }
-      }); //end 
-      
-   }); // Promise
-   }
+    return new Promise((resolve, reject) => {
+        fs.createReadStream(fileName)
+            .pipe(csvparser())
+            .on('data', function (data) {
+                try {
 
-   
-   //module.exports = readFile;
+                    if (data.date == day) {
+
+                        result.list.push(data);
+                    }
+
+                }
+
+                catch (err) {
+
+                    console.log('Error: ' + err);
+                    reject(err);
+                }
+            }) //on.data
+            .on('end', () => {
+                try {
+                    resolve(result);
+                }
+                catch (error) {
+                    console.error(error);
+                }
+            }); //end 
+
+    }); // Promise
+}
+
