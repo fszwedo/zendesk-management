1a10206ffdfccf9cbf8f3d63db8c950c0426fa3b
FIlip <filip.szwedo@gmail.com>
2022-02-13 20:30:20 +0100

    removed test console logs

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 //main file for our application
 import 'dotenv/config';
-import controller from "./src/controllers/sampleController.js";
-import makeZendeskRequest from './src/services/authenticationService.js';
 
 const app = async () => {
-    console.log('I\'m the app');
-    console.log(await makeZendeskRequest('nic', 'GET'));
 }
 
 app();
