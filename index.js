dfbbbca7b7ac82eb3729c634a01edc14584c7612
yagi527 <yrogergts@gmail.com>
2022-03-27 20:20:33 +0200

    assigning agent to ticket part2
    assigning agent to ticket part2 - final changes to our app

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-//main file for our application
-import controller from "./src/controllers/sampleController.js";
+import assignAgenttoTicketPart2 from './assignAgenttoTicketPart2.js';
 
-const app = () => {
-    console.log('I\'m the app');
-    controller();
+const sleep = ms => new Promise(r => setTimeout(r, ms));
+
+const app2 = async () => {
+
+
+    let agentLastName = '';
+
+    while (1 == 1) {
+
+        agentLastName = await assignAgenttoTicketPart2(agentLastName);
+        console.log(agentLastName);
+        await sleep(1000);
+    }
+
 }
 
-app();
+app2();
